6f5056b61cc62514fbf0575a7b5e3a4892aa4cd2
dzmitryKlimenak <48413732+dzmitryKlimenak@users.noreply.github.com>
2024-10-04 22:52:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/submit', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 7522;
+const PORT = process.env.PORT || 7580;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
